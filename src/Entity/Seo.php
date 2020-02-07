6af8c691d1662f9a512103d88f4f8a9a483eacab
Joppe de Cuyper <joppe@yappa.be>
2020-02-07 13:38:34 +0100

    Remove correct file and add back wrongly removed file

---
+++ src/Entity/Seo.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JoppeDc\SyliusBetterSeoPlugin\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Model\TranslatableInterface;
+use Sylius\Component\Resource\Model\TranslatableTrait;
+use Sylius\Component\Resource\Model\TranslationInterface;
+
+class Seo implements TranslatableInterface, ResourceInterface, SeoInterface
+{
+    use TranslatableTrait {
+        __construct as private initializeTranslationsCollection;
+        getTranslation as private doGetTranslation;
+    }
+
+    /**
+     * @var int|null
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        $this->initializeTranslationsCollection();
+    }
+
+    public function __toString()
+    {
+        return (string) $this->getId();
+    }
+
+    public function getPageTitle(): ?string
+    {
+        return $this->getTranslation()->getPageTitle();
+    }
+
+    public function setPageTitle(?string $pageTitle): void
+    {
+        $this->getTranslation()->setPageTitle($pageTitle);
+    }
+
+    public function getOgTitle(): ?string
+    {
+        return $this->getTranslation()->getOgTitle();
+    }
+
+    public function setOgTitle(?string $ogTitle): void
+    {
+        $this->getTranslation()->setOgTitle($ogTitle);
+    }
+
+    public function getOgDescription(): ?string
+    {
+        return $this->getTranslation()->getOgDescription();
+    }
+
+    public function setOgDescription(?string $ogDescription): void
+    {
+        $this->getTranslation()->setOgDescription($ogDescription);
+    }
+
+    public function getTwitterTitle(): ?string
+    {
+        return $this->getTranslation()->getTwitterTitle();
+    }
+
+    public function setTwitterTitle(?string $twitterTitle): void
+    {
+        $this->getTranslation()->setTwitterTitle($twitterTitle);
+    }
+
+    public function getTwitterDescription(): ?string
+    {
+        return $this->getTranslation()->getTwitterDescription();
+    }
+
+    public function setTwitterDescription(?string $twitterDescription): void
+    {
+        $this->getTranslation()->setTwitterDescription($twitterDescription);
+    }
+
+    public function getTwitterSite(): ?string
+    {
+        return $this->getTranslation()->getTwitterSite();
+    }
+
+    public function setTwitterSite(?string $twitterSite): void
+    {
+        $this->getTranslation()->setTwitterSite($twitterSite);
+    }
+
+    public function getExtraTags(): ?string
+    {
+        return $this->getTranslation()->getExtraTags();
+    }
+
+    public function setExtraTags(?string $extraTags): void
+    {
+        $this->getTranslation()->setExtraTags($extraTags);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getImage(): ?SeoImageInterface
+    {
+        return $this->getTranslation()->getImage();
+    }
+
+    /**
+     * @return SeoTranslation
+     */
+    public function getTranslation(?string $locale = null): TranslationInterface
+    {
+        /** @var SeoTranslation $translation */
+        $translation = $this->doGetTranslation($locale);
+
+        return $translation;
+    }
+
+    public function getTranslations(): Collection
+    {
+        return $this->translations;
+    }
+
+    protected function createTranslation(): SeoTranslation
+    {
+        return new SeoTranslation();
+    }
+}
