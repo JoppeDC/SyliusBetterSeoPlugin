26c569708d332b3f1bc9902f5e6cd95178c18562
Thomas Ferney <thomas.ferney@gmail.com>
2020-02-18 10:37:17 +0100

    Fix "The table with name 'sylius_product' already exists."

--- src/Entity/Traits/SeoTrait.php
+++ src/Entity/Traits/SeoTrait.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace JoppeDc\SyliusBetterSeoPlugin\Entity\Traits;
 
+use Doctrine\ORM\Mapping as ORM;
 use JoppeDc\SyliusBetterSeoPlugin\Entity\SeoInterface;
 
 trait SeoTrait
 {
     /**
      * @var SeoInterface|null
+     *
+     * @ORM\OneToOne(targetEntity="\JoppeDc\SyliusBetterSeoPlugin\Entity\Seo", cascade={"all"})
+     * @ORM\JoinColumn(name="seo_id", referencedColumnName="id")
      */
     protected $seo;
 
