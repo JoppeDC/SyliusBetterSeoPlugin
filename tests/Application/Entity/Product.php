658b14735927e3c715d2dd60da1a00b0b49dfa58
Thomas Ferney <thomas.ferney@gmail.com>
2020-02-25 12:46:09 +0100

    Fix Tests, resource loading and update Readme

--- tests/Application/Entity/Product.php
+++ tests/Application/Entity/Product.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
-namespace JoppeDc\SyliusBetterSeoPlugin\Entity;
+namespace Tests\JoppeDc\SyliusBetterSeoPlugin\Application\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use JoppeDc\SyliusBetterSeoPlugin\Entity\HasSeoInterface;
 use JoppeDc\SyliusBetterSeoPlugin\Entity\Traits\SeoTrait;
 use Sylius\Component\Core\Model\Product as SyliusProduct;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_product")
+ */
 class Product extends SyliusProduct implements HasSeoInterface
 {
     use SeoTrait;
